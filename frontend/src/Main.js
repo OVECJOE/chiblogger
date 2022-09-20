ad8fddb2c3111ce9828136bd258e8ae30b0eb79f
OVECJOE <ovecjoe123@gmail.com>
2022-09-20 12:50:01 +0100

    Amended the size of the Loader

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,26 +1,28 @@
-import { useContext } from 'react';
+import { useContext, lazy, Suspense } from 'react';
 import {
     BrowserRouter as Router,
     Routes, Route
 } from 'react-router-dom';
 
-import HomePage from './components/HomePage';
-import LearnPage from './components/LearnPage';
-import AboutPage from './components/AboutPage';
-import Dashboard from './components/Dashboard';
-import Profile from './components/Profile';
-import NewArticle from './components/NewArticle';
-import Articles from './components/Articles';
-import Notifications from './components/Notifications';
-import BlogArticle from './components/BlogArticle';
 import { UserContext } from './contexts/userContext';
 import { ProjectContext } from './contexts/projectContext';
-import Header from './components/Header';
 import AuthContextProvider from './contexts/authContext';
-import NotFoundPage from './components/NotFoundPage';
-import PMessage from './components/PMessage';
 import NotificationsContextProvider from './contexts/notificationsContext';
-import Notification from './components/Notification';
+import Loader from './components/misc/Loader';
+
+const Header = lazy(() => import('./components/Header'));
+const HomePage = lazy(() => import('./components/HomePage'));
+const AboutPage = lazy(() => import('./components/AboutPage'));
+const LearnPage = lazy(() => import('./components/LearnPage'));
+const Dashboard = lazy(() => import('./components/Dashboard'));
+const Profile = lazy(() => import('./components/Profile'));
+const NewArticle = lazy(() => import('./components/NewArticle'));
+const Articles = lazy(() => import('./components/Articles'));
+const Notifications = lazy(() => import('./components/Notifications'));
+const Notification = lazy(() => import('./components/Notification'));
+const BlogArticle = lazy(() => import('./components/BlogArticle'));
+const NotFoundPage = lazy(() => import('./components/NotFoundPage'));
+const PMessage = lazy(() => import('./components/PMessage'));
 
 const Main = () => {
     const { userData } = useContext(UserContext);
@@ -37,29 +39,31 @@ const Main = () => {
                 <Header />
             </AuthContextProvider>
                 <NotificationsContextProvider user={userData}>
-                    <Routes>
-                        <Route path='/' element={<HomePage />} />
-                        <Route path='/learn' element={<LearnPage />} />
-                        <Route path='/about' element={<AboutPage />} />
-                        {
-                            (userData?.isAdmin) &&
-                            <Route path='/dashboard/*' element={<Dashboard />}>
-                                <Route index element={<Notifications />} />
-                                <Route path='profile' element={<Profile />} />
-                                <Route path='new-article' element={<NewArticle />} />
-                                <Route path='articles' element={<Articles />} />
-                                <Route path='notifications/:id' element={<Notification token={userData.token} />} />
-                            </Route>
-                        }
-                        {userData?.username &&
-                            <Route path='articles/:slugName' element={<BlogArticle />} />
-                        }
-                        <Route path='*'
-                            element={<NotFoundPage
-                                notFound={true}
-                            />}
-                            />
-                    </Routes>
+                    <Suspense fallback={<Loader width='100vw' height='100vh' />}>
+                        <Routes>
+                            <Route path='/' element={<HomePage />} />
+                            <Route path='/learn' element={<LearnPage />} />
+                            <Route path='/about' element={<AboutPage />} />
+                            {
+                                (userData?.isAdmin) &&
+                                <Route path='/dashboard/*' element={<Dashboard />}>
+                                    <Route index element={<Notifications />} />
+                                    <Route path='profile' element={<Profile />} />
+                                    <Route path='new-article' element={<NewArticle />} />
+                                    <Route path='articles' element={<Articles />} />
+                                    <Route path='notifications/:id' element={<Notification token={userData.token} />} />
+                                </Route>
+                            }
+                            {userData?.username &&
+                                <Route path='articles/:slugName' element={<BlogArticle />} />
+                            }
+                            <Route path='*'
+                                element={<NotFoundPage
+                                    notFound={true}
+                                />}
+                                />
+                        </Routes>
+                    </Suspense>
                 </NotificationsContextProvider>
         </Router>
     );
--- frontend/src/components/misc/Loader.js
+++ frontend/src/components/misc/Loader.js
@@ -1,47 +1,47 @@
 import { motion } from "framer-motion";
 
-const loadingContainer = {
-    width: "20px",
-    height: "20px",
-    display: "flex",
-    justifyContent: "space-around",
-};
-const loadingCircle = {
-    display: "block",
-    width: "1rem",
-    height: "1rem",
-    backgroundColor: "#3A36DB",
-    borderRadius: "0.5rem",
-};
-
-const loadingContainerVariants = {
-    start: {
-        transition: {
-            staggerChildren: 0.2,
+const Loader = ({ width, height }) => {
+    const loadingContainer = {
+        width: width || "50px",
+        height: height || "30px",
+        display: "flex",
+        justifyContent: "space-around",
+    };
+    const loadingCircle = {
+        display: "block",
+        width: "1rem",
+        height: "1rem",
+        backgroundColor: "#3A36DB",
+        borderRadius: "0.5rem",
+    };
+    
+    const loadingContainerVariants = {
+        start: {
+            transition: {
+                staggerChildren: 0.2,
+            },
         },
-    },
-    end: {
-        transition: {
-            staggerChildren: 0.2,
+        end: {
+            transition: {
+                staggerChildren: 0.2,
+            },
         },
-    },
-};
+    };
+    
+    const loadingCircleVariants = {
+        start: {
+            y: "0%",
+        },
+        end: {
+            y: "60%",
+        },
+    };
+    const loadingCircleTransition = {
+        duration: 0.4,
+        yoyo: Infinity,
+        ease: 'easeInOut'
+    }
 
-const loadingCircleVariants = {
-    start: {
-        y: "0%",
-    },
-    end: {
-        y: "60%",
-    },
-};
-const loadingCircleTransition = {
-    duration: 0.4,
-    yoyo: Infinity,
-    ease: 'easeInOut'
-}
-
-const Loader = () => {
     return (
         <div>
             <motion.div
