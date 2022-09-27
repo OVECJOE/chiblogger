0de38bbb4d2437f29c5bdc02b7644d5a93e7cef6
Victor Ohachor <vohachor@gmail.com>
2022-09-27 03:35:56 +0100

    Deploying to heroku

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./config/database').connect();
 
 const express = require('express');
+const path = require('path');
 
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
@@ -14,6 +15,25 @@ app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/articles', postsRouter);
 app.use('/api/notifications', notificationsRouter);
+
+// ---------------------DEPLOYMENT CODE-------------------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname1, 'frontend', 'build', 'index.html')
+        );
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is Running Successfully');
+    });
+}
+// ---------------------DEPLOYMENT CODE-------------------
+
 
 app.get('*', (req, res) => {
     res.status(404).send({
